4f786dd1c9eae937e391269a0690fcc94ab4887a
Simone Margio <simone.margio@icloud.com>
2023-03-20 16:02:36 +0100

    Exercises 51 to 56

---
+++ 01-Basic-JavaScript/051-Return a Value from a Function with Return.js
@@ -0,0 +1,26 @@
+/*
+
+Created by Simone Margio
+www.simonemargio.im
+
+We can pass values into a function with arguments. You can use a return statement to send a value back out of a function.
+
+Example
+
+function plusThree(num) {
+  return num + 3;
+}
+
+const answer = plusThree(5);
+answer has the value 8.
+
+plusThree takes an argument for num and returns a value equal to num + 3.
+Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+*/
+
+
+// Solution
+function timesFive(number) {
+    return number * 5;
+  }
