85848fd47dc69c8881e784b7804a48816d12edef
Simone Margio <simone.margio@icloud.com>
2023-03-20 16:01:15 +0100

    Exercises 31 to 40

---
+++ 01-Basic-JavaScript/036-Use Bracket Notation to Find the Last Character in a String.js
@@ -0,0 +1,29 @@
+/*
+
+Created by Simone Margio
+www.simonemargio.im
+
+In order to get the last letter of a string, you can subtract one from the string's length.
+
+For example, if const firstName = "Ada", you can get the value of the last letter of the string by using firstName[firstName.length - 1].
+
+Example:
+
+const firstName = "Ada";
+const lastLetter = firstName[firstName.length - 1];
+lastLetter would have a value of the string a.
+Use bracket notation to find the last character in the lastName variable.
+
+Hint: Try looking at the example above if you get stuck.
+
+*/
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName; // Change this line
+
+
+// Solution
+const lastLetterOfLastName = lastName[lastName.length - 1]; 
---
+++ 01-Basic-JavaScript/040-Nest one Array within Another Array.js
@@ -0,0 +1,19 @@
+/*
+
+Created by Simone Margio
+www.simonemargio.im
+
+You can also nest arrays within other arrays, like below:
+
+const teams = [["Bulls", 23], ["White Sox", 45]];
+This is also called a multi-dimensional array.
+Create a nested array called myArray.
+
+*/
+
+// Only change code below this line
+const myArray = [];
+
+
+// Solution
+const myArray = [["Simone", 1], ["Sol", 2]];
