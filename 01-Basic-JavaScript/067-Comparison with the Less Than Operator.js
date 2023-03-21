12f11e3e4304fc070fde9295dd369bd82a175ece
Simone Margio <simone.margio@icloud.com>
2023-03-21 11:30:00 +0100

    Exercise 67

---
+++ 01-Basic-JavaScript/067-Comparison with the Less Than Operator.js
@@ -0,0 +1,47 @@
+/*
+
+Created by Simone Margio
+www.simonemargio.im
+
+The less than operator (<) compares the values of two numbers. If the number to the left is less than the number to the right, it returns true. Otherwise, it returns false. Like the equality operator, the less than operator converts data types while comparing.
+
+Examples
+
+2   < 5 // true
+'3' < 7 // true
+5   < 5 // false
+3   < 2 // false
+'8' < 4 // false
+Add the less than operator to the indicated lines so that the return statements make sense.
+
+*/
+
+function testLessThan(val) {
+    if (val) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  testLessThan(10);
+
+
+// Solution
+function testLessThan(val) {
+    if (val < 25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val < 55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  testLessThan(10);
