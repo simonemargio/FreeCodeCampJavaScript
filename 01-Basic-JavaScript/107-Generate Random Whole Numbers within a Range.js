cea3afca78c529d5b744103fea3452cd00adc06b
Simone Margio <simone.margio@icloud.com>
2023-03-25 18:13:57 +0100

    Exercise 107

---
+++ 01-Basic-JavaScript/107-Generate Random Whole Numbers within a Range.js
@@ -0,0 +1,27 @@
+/*
+
+Created by Simone Margio
+www.simonemargio.im
+
+Instead of generating a random whole number between zero and a given number like we did before, we can generate a random whole number that falls within a range of two specific numbers.
+
+To do this, we'll define a minimum number min and a maximum number max.
+
+Create a function called randomRange that takes a range myMin and myMax and returns a random whole number that's greater than or equal to myMin, and is less than or equal to myMax, inclusive.
+
+
+return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+
+
+*/
+
+function randomRange(myMin, myMax) {
+    // Only change code below this line
+    return 0;
+    // Only change code above this line
+  }
+
+
+
+// Solution
+return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
