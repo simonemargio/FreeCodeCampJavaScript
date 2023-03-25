d802540e8b1fde22d03e9f1cd440eb195d1de453
Simone Margio <simone.margio@icloud.com>
2023-03-25 18:55:09 +0100

    Exercise 113

---
+++ 01-Basic-JavaScript/113-Use Recursion to Create a Range of Numbers.js
@@ -0,0 +1,26 @@
+/*
+
+Created by Simone Margio
+www.simonemargio.im
+
+Continuing from the previous challenge, we provide you another opportunity to create a recursive function to solve a problem.
+We have defined a function named rangeOfNumbers with two parameters. The function should return an array of integers which begins with a number represented by the startNum parameter and ends with a number represented by the endNum parameter. The starting number will always be less than or equal to the ending number. Your function must use recursion by calling itself and not use loops of any kind. It should also work for cases where both startNum and endNum are the same.
+
+*/
+
+function rangeOfNumbers(startNum, endNum) {
+    return [];
+  };
+
+
+// Solution
+function rangeOfNumbers(startNum, endNum) {
+    if (startNum == endNum) {
+      return [endNum];
+    } else {
+      let arr = rangeOfNumbers(startNum + 1, endNum);
+      arr.unshift(startNum);
+      return arr;
+    }
+  };
+  
