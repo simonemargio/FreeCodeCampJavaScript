cf2ef7a5f519ee16d08cc8be123cdf7e9f7b8548
Simone Margio <simone.margio@icloud.com>
2023-04-27 20:19:00 +0200

    Exercise 22

---
+++ 03-Regular-Expressions/22-Restrict Possible Usernames.js
@@ -0,0 +1,24 @@
+/*
+
+Created by Simone Margio
+www.simonemargio.im
+
+Usernames are used everywhere on the internet. They are what give users a unique identity on their favorite sites.
+
+You need to check all the usernames in a database. Here are some simple rules that users have to follow when creating their username.
+
+Usernames can only use alpha-numeric characters.
+The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+Username letters can be lowercase and uppercase.
+Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
+Change the regex userCheck to fit the constraints listed above.
+
+*/
+
+let username = "JackOfAllTrades";
+let userCheck = /change/; // Change this line
+let result = userCheck.test(username);
+
+
+// Solution
+let userCheck = /^[a-z]([0-9]{2,}|[a-z]+\d*)$/i;
